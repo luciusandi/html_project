60894dd1f82797dda81c65046dae35a3022dfeb9
Lucius Andi Putra <Lucius.andi@gmail.com>
2018-12-01 16:21:41 +0800

    day 5

---
+++ day5/home.js
@@ -0,0 +1,41 @@
+var my_name;
+var my_age;
+var my_birthdate;
+
+my_name = "Stackup";
+my_age = 23;
+my_birthdate = "20/03/1988";
+
+console.log(my_name);
+console.log(my_age);
+console.log(my_birthdate);
+
+var my_address = "JTC LaunchPad";
+
+var my_test;
+
+console.log( typeof(my_test) );
+console.log( typeof(my_name) );
+console.log( typeof(my_age) );
+
+var firstName;
+var lastName;
+var space;
+
+firstName = "Nick";
+lastName = "Test";
+space = " ";
+
+var fullName = firstName + space + lastName;
+
+var fullName1 = firstName + 5 + lastName; 
+
+console.log(fullName);
+console.log(fullName1);
+
+var qty = 2;
+var total = 200;
+var qtyTotal = qty + total;
+
+console.log(qtyTotal);
+
